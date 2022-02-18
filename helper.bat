b7d8c21fbb6f0a19bfb0580736b6de7bfa37a391
Ricardo <ricardinhogiasson16@gmail.com>
2022-02-18 15:19:16 -0300

    Finalizando Responsividade

--- helper.bat
+++ helper.bat
@@ -1,1 +1,2 @@
 print.exe lpt1  C:\Users\User\Desktop\ZEBRA-NODE\Etiqueta.prn
+
--- index.js
+++ index.js
@@ -7,13 +7,27 @@ const etiquetaFinal = "./Etiqueta.prn";
 const executavel = "C:\\Users\\User\\Desktop\\ZEBRA-NODE\\helper.bat"
 
 
-// exec('./helper.bat', (err, stdout, stderr) => {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-//     console.log(stdout);
-//   });
+function defineEspacamentoBarras(cod) {
+    let nCasasBarCode = 110;
+    if (cod.length > 1) {
+        for (let index = 0; index < cod.length; index++) {
+            nCasasBarCode = nCasasBarCode - 10;
+
+        }
+    }
+    return nCasasBarCode;
+}
+
+function defineEspacamentoObjeto(cod){
+    let nCasas = 135;
+    if (cod.length > 1) {
+        for (let index = 0; index < cod.length; index++) {
+            nCasas = nCasas - 2;
+
+        }
+    }
+    return nCasas;
+}
 async function executa() {
     fs.readFile(parametros, 'utf8', function (err, data) {
         //console.log(data);
@@ -24,44 +38,54 @@ async function executa() {
         for (let index = 0; index < lista.length; index++) {
             // 
 
-           async function teste(){
-                await new Promise(r => setTimeout(()=>{
+            async function teste() {
+                await new Promise(r => setTimeout(() => {
                     console.log("Olha o timeout")
                     const element = lista[index];
                     const codigo_e_nome = element.split(",");
 
-            console.log(codigo_e_nome[0])// CÓDIGO
-            console.log(codigo_e_nome[1])// NOME
+                    console.log(codigo_e_nome[0])// CÓDIGO
+                    console.log(codigo_e_nome[1])// NOME
 
-            fs.readFile(prototipo, 'utf8', function (err, data) {
-                if (err) throw err;
-                data = data.replace("Codigo", codigo_e_nome[0]);
-                data = data.replace("Objeto", codigo_e_nome[1]);
-                console.log(data)
-
-                fs.writeFile(etiquetaFinal, data, (err) => {
-                    if (err) return console.log(err);
-                   // await delay(1000)
-                     
-                      exec(executavel, (err, stdout, stderr) => {
-                          if (err) {
-                              console.error(err);
-                              return;
-                          }
-                          console.log(stdout);
-                      });
+                    fs.readFile(prototipo, 'utf8', function (err, data) {
 
 
-                });
 
 
-            });
+
+
+                        if (err) throw err;
+                        let nCasasBarCode = defineEspacamentoBarras(codigo_e_nome[0]);
+                        let nCasasObjeto =defineEspacamentoObjeto(codigo_e_nome[1]); 
+                        data = data.replace("^FTO", `^FT${nCasasBarCode},63^A0N,20,20^FH\^CI28^FDObjeto^FS^CI27`)
+                        data = data.replace("Codigo", codigo_e_nome[0]);
+                        data = data.replace("Objeto", codigo_e_nome[1]);
+                        data = data.replace("^BY", `^BY2,3,72^FT${nCasasBarCode},142^BCN,,Y,N`);
+                        console.log(data)
+
+                        fs.writeFile(etiquetaFinal, data, (err) => {
+                            if (err) return console.log(err);
+                            // await delay(1000)
+
+                            exec(executavel, (err, stdout, stderr) => {
+                                if (err) {
+                                    console.error(err);
+                                    return;
+                                }
+                                console.log(stdout);
+                            });
+
+
+                        });
+
+
+                    });
                 }, 2000 * (index + 1)));
                 //
 
-            
+
             }
-            
+
 
             teste()
         }
