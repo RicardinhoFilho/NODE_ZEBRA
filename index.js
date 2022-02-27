df28dcb5325bf4800a5194411ffd3210c63de720
Ricardo <ricardinhogiasson16@gmail.com>
2022-02-27 08:10:37 -0300

    Correções codigo

--- index.js
+++ index.js
@@ -4,97 +4,103 @@ const fs = require('fs');
 const parametros = "./parametros.txt";
 const prototipo = "./prototipo.txt";
 const etiquetaFinal = "./Etiqueta.prn";
-const executavel = "C:\\Users\\User\\Desktop\\ZEBRA-NODE\\helper.bat"
+let pastaTXT = "./pasta.txt"
+let executavel = ""
+
+
+
+
 
 //DEFINE ESPAÇAMENTO BARCODE - Começando com 110 e diminuindo 10 para cada caractere
-function defineEspacamentoBarras(cod) {
-    let nCasasBarCode = 110;
-    if (cod.length > 1) {
-        for (let index = 0; index < cod.length; index++) {
-            nCasasBarCode = nCasasBarCode - 10;
-
-        }
+function formataBarCode(cod) {
+    if (!cod) {
+        throw 'Código undefined';
     }
-    return nCasasBarCode;
-}
-//DEFINE ESPAÇAMENTO Descrução - Começando com 135 e diminuindo 5 para cada caractere
-function defineEspacamentoObjeto(cod) {
-    let nCasas = 135;
-    if (cod.length > 1) {
-        for (let index = 0; index < cod.length; index++) {
-            nCasas = nCasas - 2;
 
-        }
-    }
-    return nCasas;
+    let teste = '0000000000';
+    return teste.substring(0, teste.length - cod.length) + cod;
 }
+
 async function executa() {
-    fs.readFile(parametros, 'utf8', function (err, data) {
-        //console.log(data);
-
-        let lista = data.split("\n")
-
-        // lista.forEach((element) => {
-        for (let index = 0; index < lista.length; index++) {
-            // 
-
-            async function teste() {
-                await new Promise(r => setTimeout(() => {
-                    console.log("Olha o timeout")
-                    const element = lista[index];
-                    const codigo_e_nome = element.split(",");
-
-                    console.log(codigo_e_nome[0])// CÓDIGO
-                    console.log(codigo_e_nome[1])// NOME
-
-                    fs.readFile(prototipo, 'utf8', function (err, data) {
+    fs.readFile(pastaTXT, 'utf8', function (err, data) {
+        console.log(data)
+        executavel = data + 'helper.bat';
+        console.log(executavel);
 
 
 
+        fs.readFile(parametros, 'utf8', function (err, data) {
+            //console.log(data);
+
+            let lista = data.split("\n")
+
+            // lista.forEach((element) => {
+            for (let index = 0; index < lista.length; index++) {
+                // 
+
+                async function teste() {
+                    await new Promise(r => setTimeout(() => {
+                        console.log("Olha o timeout")
+                        const element = lista[index];
+                        const codigo_e_nome = element.split(",");
+
+                        console.log(codigo_e_nome[0])// CÓDIGO
+                        console.log(codigo_e_nome[1])// NOME
+
+                        fs.readFile(prototipo, 'utf8', function (err, data) {
 
 
 
-                        if (err) throw err;
-                        let nCasasBarCode = defineEspacamentoBarras(codigo_e_nome[0]);
-                        let nCasasObjeto = defineEspacamentoObjeto(codigo_e_nome[1]);
-                        data = data.replace("^FTO", `^FT${nCasasBarCode},63^A0N,20,20^FH\^CI28^FDObjeto^FS^CI27`)
-                        data = data.replace("Codigo", codigo_e_nome[0]);
-                        data = data.replace("Objeto", codigo_e_nome[1]);
-                        data = data.replace("^BY", `^BY2,3,72^FT${nCasasBarCode},142^BCN,,Y,N`);
-                        console.log(data)
 
-                        fs.writeFile(etiquetaFinal, data, (err) => {
-                            if (err) return console.log(err);
-                            // await delay(1000)
+                            console.log('objeto', codigo_e_nome[1].length)
 
-                            exec(executavel, (err, stdout, stderr) => {
-                                if (err) {
-                                    console.error(err);
-                                    return;
-                                }
-                                console.log(stdout);
+                            if (err) throw err;
+                            const codigo = formataBarCode(codigo_e_nome[0]);
+
+                            data = data.replace("Codigo", codigo);
+                            // data = data.replace("Objeto1", 
+                            // `^FT55,58^A0N,17,18^FH\^CI28^FD${codigo_e_nome[1].substring(0, 25)}^FS^CI27`);
+                            // data = data.replace("Objeto2",`^FT55,78^A0N,17,18^FH\^CI28^FD${codigo_e_nome[1].substring(25, 50)}^FS^CI27`);
+                            data = data.replace("Objeto1",codigo_e_nome[1].substring(0, 25));
+                            data = data.replace("Objeto2", codigo_e_nome[1].substring(25, 50));
+
+                            console.log(data)
+
+                            fs.writeFile(etiquetaFinal, data, (err) => {
+                                if (err) return console.log(err);
+                                // await delay(1000)
+
+                                exec(executavel, (err, stdout, stderr) => {
+                                    if (err) {
+                                        console.error(err);
+                                        return;
+                                    }
+                                    console.log(stdout);
+                                });
+
+
                             });
 
 
                         });
+                    }, 2000 * (index + 1)));
+                    //
 
 
-                    });
-                }, 2000 * (index + 1)));
-                //
+                }
 
 
+                teste()
             }
 
 
-            teste()
-        }
+        });
 
+        // });
 
-    });
-
-    // });
-
-
+    })
 }
+
 executa();
+
+console.log(executavel)
